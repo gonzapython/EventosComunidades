a5810bd9a1d726fda5ac5ffe9c0ee02e3130fce9
gonzapython <56170708+gonzapython@users.noreply.github.com>
2019-11-10 10:37:39 +0100

    especificaciones

--- clases.py
+++ clases.py
@@ -1,18 +1,19 @@
-
 class Usuarios():
-    def __init__(self, nombre, contrasena):
-        self.nombre     = nombre
-        self.contrasena = contrasena
+    def __init__(self, nombre, contrasena, es_creador, es_organizador):
+        self.nombre         = nombre
+        self.contrasena     = contrasena
+        self.es_creador     = es_creador
+        self.es_organizador = es_organizador
 #
 usuario = Usuarios(self.nombre, self.contrasena)
 
 class Comunidades():
     def __init__(self, nombre, direccion, fecha_creacion, creador, organizador):
-        self.nombre = nombre
-        self.direccion = direccion
+        self.nombre         = nombre
+        self.direccion      = direccion
         self.fecha_creacion = fecha_creacion
-        self.creador = creador
-        self.organizador = organizador
+        self.creador        = creador
+        self.organizador    = organizador
 #
 comunidad = Comunidades(self.nombre, self.direccion, self.fecha_creacion, self.creador, self.organizador)
 
@@ -23,17 +24,3 @@ class Eventos():
         self.fecha       = fecha
 #
 evento = Eventos(self.nombre, self.descripcion, self.fecha)
-
-class MiembrosComunidad():
-    def __init__(self, nombre, comunidad):
-        self.nombre    = usuario.nombre
-        self.comunidad = comunidad.nombre
-#
-miembrocomunidad = MiembrosComunidad(self.nombre, self.comunidad)
-
-class AsistentesEventos():
-    def __init__(self, asistente, evento):
-        self.asistente = usuario.nombre
-        self.evento    = evento.nombre
-#
-asistenteevento = AsistentesEventos(self.evento, self.evento)
