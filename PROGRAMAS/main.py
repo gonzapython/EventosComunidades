fa0b8d27a528a35fdeb28ec34c862d84006c030a
gonzapython <56170708+gonzapython@users.noreply.github.com>
2019-11-13 11:48:42 +0100

    lo mismo

--- PROGRAMAS/main.py
+++ PROGRAMAS/main.py
@@ -1,5 +1,6 @@
 from usuario   import Usuarios
 from comunidad import ComunidadesUsuario
+from precomunidad import Comunidades
 from evento    import EventosUsuarioComunidad
 
 #usuario   = Usuarios()
@@ -12,14 +13,21 @@ usuario   = Usuarios('jose','aaaaaa', 'c', 'o', 'comunidad2')
 evento    = EventosUsuarioComunidad('hackeando cañas',
                                     'lalalala', 'fecha',
                                      usuario)
-comunidad = ComunidadesUsuario('OSW','calle de la luz','10/10/10', usuario, evento)
+#comunidad = ComunidadesUsuario('OSW','calle de la luz','10/10/10', usuario, evento)
 
+comunidad = Comunidades('OSW','calle de la luz','10/10/10')
 
+# -- para que 'comunidad' sea un objeto (no un str) y
+# -- pueda usarlo para enlazar el usuario con la comunidad
+# -- de usuario.py a comunidad.py : self.comunidadusuario.opciones_comunidad(self.nombre)
+# --
+comunidadusuario = ComunidadesUsuario('OSW','calle de la luz','10/10/10', usuario, comunidad, evento)
+# --
 
 
 def main():
     usuario.login()
-    
+
 
 # -----
 main()
---
+++ PROGRAMAS/precomunidad.py
@@ -0,0 +1,7 @@
+class Comunidades():
+    def __init__(self, nombre, direccion, fecha_creacion):
+        self.nombre         = nombre
+        self.direccion      = direccion
+        self.fecha_creacion = fecha_creacion
+
+
